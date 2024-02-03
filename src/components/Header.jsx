12629f5cee2560d4a3373c33c945f8ea49674e80
Goodluck Japhet <goodluckjaphet7@gmail.com>
2024-02-03 14:29:48 +0300

    logo size chang e

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,18 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-        <header>
+        <header className='flex justify-between items-center
+        px-3 max-w-6xl mx-auto'>
             <div>
-                <img src="https://github.com/GoldenCicada1/rgpapp-react/blob/main/public/logo-blue.png?raw=true" 
-                alt="logo" 
-                className="h-5 cursor-pointer"/>
+                <img src='https://github.com/GoldenCicada1/rgpapp-react/blob/main/public/logo-blue.png?raw=true' 
+                alt='logo' className='h-4 cursor-pointer' />
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Offers</li>
+                    <li>Sign in</li>
+                </ul>
             </div>
         </header>
     </div>
